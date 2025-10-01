5e9373e0ade3bf95679f6985254025e4d19ec296
Vlad Tihomirov <7292963@mail.ru>
2025-10-01 12:02:46 +0300

    Playwright test

---
+++ Python_RESTAPI_Playwright_Gherkin/Playwright/Try_testE2E/test_todo_e2e.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+def test_can_add_todo():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://demo.playwright.dev/todomvc")
+        page.fill(".new-todo","Протестировать платёж")
+        page.press(".new-todo","Enter")
+        page.wait_for_selector("text=Протестировать платёж")
+        # assert page.is_visible(".new-todo")
+        browser.close()
